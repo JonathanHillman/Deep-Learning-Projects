48669a767f9016bcd6f0fcf162d309cd312ade41
Jonathan Hillman <jonathandalehillman@gmail.com>
2020-06-29 01:59:12 -0700

    Add files via upload

---
+++ GradientDescent.R
@@ -0,0 +1,289 @@
+
+#main <- function()
+#{
+  #library and helper function
+  source("GradientDescentHelper.R")
+  set.seed(8)
+  
+  #initialize global variables
+  numIterVector = 1:(maxIterations + 1)
+  
+  #############################################################################
+  #############################################################################
+  #                             First Data Set                                #
+  #############################################################################
+  #############################################################################
+  #import data as a list
+  import_data <- fread('https://web.stanford.edu/~hastie/ElemStatLearn/datasets/spam.data')
+  #convert list to matrix
+  import_data <- matrix( unlist(import_data), nrow(import_data), ncol(import_data) )
+  
+  #randomized list of 0's 1's and 2's
+  classificationList <- createClassificationList(import_data)
+  
+  #make X and Y vector/matrix
+  y <- import_data[,58]
+  X <- import_data[,1:57]
+  X <- scale(X)
+  
+  #separate data using classificationList as needed
+  X_train <- X[classificationList == 0,]
+  X_validation <- X[classificationList == 1,]
+  X_test <- X[classificationList == 2,]
+  
+  #separate y using 
+  y_train <- y[classificationList == 0]
+  y_validation <- y[classificationList == 1]
+  y_test <- y[classificationList == 2]
+  
+  #print stuff for the report
+  print("0     1")
+  print(c("test",length(y_test[y_test==0]),length(y_test[y_test==1])))
+  print(c("train",length(y_train[y_train==0]),length(y_train[y_train==1])))
+  print(c("validation",length(y_validation[y_validation==0]),length(y_validation[y_validation==1])))
+  
+  #store weightMatrix
+  weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
+  
+  #initialize vector variables
+  trainErrorVector = NA
+  validationErrorVector = NA
+  testErrorVector = NA
+  trainMeanLogLossVector = NA
+  validationMeanLogLossVector = NA 
+  #update vector variables
+  for(i in 1:(maxIterations+1))
+  {
+    trainErrorVector[i] <- 1 - findAccuracy( X_train, y_train, weightMatrix[,i])
+    validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
+    testErrorVector[i] <- 1 - findAccuracy( X_test, y_test, weightMatrix[,i])
+    trainMeanLogLossVector[i] <- calculateMeanLogLoss( X_train, y_train, weightMatrix[,i] )
+    validationMeanLogLossVector[i] <- calculateMeanLogLoss( X_validation, y_validation, weightMatrix[,i] )
+  }
+  
+  #print stuff for tables
+  trainMinError = 1 - findAccuracy(X_train, y_train, weightMatrix[,which.min(trainErrorVector)])
+  validationMinError = 1 - findAccuracy(X_validation, y_validation, weightMatrix[,which.min(validationErrorVector)])
+  testMinError = 1 - findAccuracy(X_test, y_test, weightMatrix[,which.min(testErrorVector)])
+  print("min log error for train, validation, test")
+  print(trainMinError)
+  print(validationMinError)
+  print(testMinError)
+  print("baseline for train, validation, test (subtract from 1 if needed)")
+  print(length(y_train[y_train==1])/length(y_train))
+  print(length(y_validation[y_validation==1])/length(y_validation))
+  print(length(y_test[y_test==1])/length(y_test))
+  
+  #plot how %error and mean log loss for data set
+  programPlot(numIterVector,trainErrorVector,validationErrorVector,trainMeanLogLossVector,validationMeanLogLossVector, weightMatrix)
+  
+  #ROC curve implementation
+  TPR <- NA
+  FPR <- NA
+  for(p in 1:10000)
+  {
+    y.hat <- predictY(y_test, X_test, weightMatrix[,which.min(testErrorVector)], (p/10000))
+    ROC.df <- WeightedROC(y.hat,y_test)
+    TPR[p] <- ROC.df[2,'TPR']
+    FPR[p] <- ROC.df[2,'FPR']
+  }
+  
+  roctest<- ggplot()+
+    geom_path(aes(x=FPR, y=TPR, color="ROC"))+
+    geom_path(aes(x=c(0,1),y=c(0,1), color="base"))+
+    scale_color_manual(values=c(ROC="blue", base="violet"))+
+    labs(color="model")
+   
+   
+ 
+  print(roctest)
+  #############################################################################
+  #############################################################################
+  #                              Second Data Set                              #
+  #############################################################################
+  #############################################################################
+  #import data as a list
+  import_data <- fread('https://web.stanford.edu/~hastie/ElemStatLearn/datasets/SAheart.data')
+  import_data[,6][import_data[,6] == "Present"] = 1
+  import_data[,6][import_data[,6] == "Absent"] = 0
+  #convert list to matrix
+  import_data <- matrix( as.numeric(unlist(import_data)), nrow(import_data), ncol(import_data) )
+  
+  #randomized list of 0's 1's and 2's
+  classificationList <- createClassificationList(import_data)
+  
+  #make X and Y vector/matrix
+  y <- import_data[,ncol(import_data)]
+  X <- import_data[,2:10]
+  X <- clean(X)
+  X <- scale(X)
+  
+  #separate data using classificationList as needed
+  X_train <- X[classificationList == 0,]
+  X_validation <- X[classificationList == 1,]
+  X_test <- X[classificationList == 2,]
+  
+  #separate y using 
+  y_train <- y[classificationList == 0]
+  y_validation <- y[classificationList == 1]
+  y_test <- y[classificationList == 2]
+  
+  #print stuff for report
+  print("0     1")
+  print(c("test",length(y_test[y_test==0]),length(y_test[y_test==1])))
+  print(c("train",length(y_train[y_train==0]),length(y_train[y_train==1])))
+  print(c("validation",length(y_validation[y_validation==0]),length(y_validation[y_validation==1])))
+  
+  #store weightMatrix
+  weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
+  
+  #initialize vector variables
+  trainErrorVector = NA
+  validationErrorVector = NA
+  testErrorVector = NA
+  trainMeanLogLossVector = NA
+  validationMeanLogLossVector = NA
+  #update vector variables
+  for(i in 1:(maxIterations+1))
+  {
+    trainErrorVector[i] <- 1 - findAccuracy( X_train, y_train, weightMatrix[,i])
+    validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
+    testErrorVector[i] <- 1 - findAccuracy( X_test, y_test, weightMatrix[,i])
+    trainMeanLogLossVector[i] <- calculateMeanLogLoss( X_train, y_train, weightMatrix[,i] )
+    validationMeanLogLossVector[i] <- calculateMeanLogLoss( X_validation, y_validation, weightMatrix[,i] )
+  }
+  
+  #print stuff for tables
+  trainMinError = 1 - findAccuracy(X_train, y_train, weightMatrix[,which.min(trainErrorVector)])
+  validationMinError = 1 - findAccuracy(X_validation, y_validation, weightMatrix[,which.min(validationErrorVector)])
+  testMinError = 1 - findAccuracy(X_test, y_test, weightMatrix[,which.min(testErrorVector)])
+  print("min log error for train, validation, test")
+  print(trainMinError)
+  print(validationMinError)
+  print(testMinError)
+  print("baseline for train, validation, test (subtract from 1 if needed)")
+  print(length(y_train[y_train==1])/length(y_train))
+  print(length(y_validation[y_validation==1])/length(y_validation))
+  print(length(y_test[y_test==1])/length(y_test))
+  
+  #plot how %error and mean log loss for data set
+  programPlot(numIterVector,trainErrorVector,validationErrorVector,trainMeanLogLossVector,validationMeanLogLossVector, weightMatrix)
+  
+  #ROC curve implementation
+  TPR <- NA
+  FPR <- NA
+  for(p in 1:100)
+  {
+    y.hat <- predictY(y_test, X_test, weightMatrix[,which.min(testErrorVector)], (p/100))
+    ROC.df <- WeightedROC(y.hat,y_test)
+    TPR[p] <- ROC.df[2,'TPR']
+    FPR[p] <- ROC.df[2,'FPR']
+  }
+  
+  roctest<- ggplot()+
+    geom_line(aes(x=FPR, y=TPR, color="ROC"))+
+    geom_path(aes(x=c(0,1),y=c(0,1), color="base"))+
+    scale_color_manual(values=c(ROC="blue", base="violet"))+
+    labs(color="model")
+  
+  
+  
+  print(roctest)
+  #############################################################################
+  #############################################################################
+  #                             Third Data Set                                #
+  #############################################################################
+  #############################################################################
+  #import data as a list
+  import_data <- fread('zip.train')
+  import_data <- matrix(as.numeric(unlist(import_data)), nrow(import_data), ncol(import_data))
+  import_data <- rbind(import_data[import_data[,1] == 0,], import_data[import_data[,1] == 1,])
+  
+  #randomized list of 0's 1's and 2's
+  classificationList <- createClassificationList(import_data)
+  
+  #make X and Y vector/matrix
+  y <- import_data[,1]
+  X <- import_data[,2:257]
+  X <- clean(X)
+  X <- scale(X)
+  
+  #separate data using classificationList as needed
+  X_train <- X[classificationList == 0,]
+  X_validation <- X[classificationList == 1,]
+  X_test <- X[classificationList == 2,]
+  
+  #separate y using 
+  y_train <- y[classificationList == 0]
+  y_validation <- y[classificationList == 1]
+  y_test <- y[classificationList == 2]
+  
+  #print stuff for report
+  print("0     1")
+  print(c("test",length(y_test[y_test==0]),length(y_test[y_test==1])))
+  print(c("train",length(y_train[y_train==0]),length(y_train[y_train==1])))
+  print(c("validation",length(y_validation[y_validation==0]),length(y_validation[y_validation==1])))
+  
+  #store weightMatrix
+  weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
+  
+  #initialize vector variables
+  trainErrorVector = NA
+  validationErrorVector = NA
+  testErrorVector = NA
+  trainMeanLogLossVector = NA
+  validationMeanLogLossVector = NA 
+  #update vector variables
+  for(i in 1:(maxIterations+1))
+  {
+    trainErrorVector[i] <- 1 - findAccuracy( X_train, y_train, weightMatrix[,i])
+    validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
+    testErrorVector[i] <- 1 - findAccuracy( X_test, y_test, weightMatrix[,i])
+    trainMeanLogLossVector[i] <- calculateMeanLogLoss( X_train, y_train, weightMatrix[,i] )
+    validationMeanLogLossVector[i] <- calculateMeanLogLoss( X_validation, y_validation, weightMatrix[,i] )
+  }
+  
+  #print stuff for pdf
+  trainMinError = 1 - findAccuracy(X_train, y_train, weightMatrix[,which.min(trainErrorVector)])
+  validationMinError = 1 - findAccuracy(X_validation, y_validation, weightMatrix[,which.min(validationErrorVector)])
+  testMinError = 1 - findAccuracy(X_test, y_test, weightMatrix[,which.min(testErrorVector)])
+  print("min log error for train, validation, test")
+  print(trainMinError)
+  print(validationMinError)
+  print(testMinError)
+  print("baseline for train, validation, test (subtract from 1 if needed)")
+  print(length(y_train[y_train==1])/length(y_train))
+  print(length(y_validation[y_validation==1])/length(y_validation))
+  print(length(y_test[y_test==1])/length(y_test))
+  
+  #plot how %error and mean log loss for data set
+  programPlot(numIterVector,trainErrorVector,validationErrorVector,trainMeanLogLossVector,validationMeanLogLossVector, weightMatrix)
+  
+  #ROC curve implementation
+  TPR <- NA
+  FPR <- NA
+  for(p in 1:10000)
+  {
+    y.hat <- predictY(y_test, X_test, weightMatrix[,which.min(testErrorVector)], (p/10000))
+    ROC.df <- WeightedROC(y.hat,y_test)
+    TPR[p] <- ROC.df[2,'TPR']
+    FPR[p] <- ROC.df[2,'FPR']
+  }
+  
+  roctest<- ggplot()+
+    geom_path(aes(x=FPR, y=TPR, color="ROC"))+
+    geom_path(aes(x=c(0,1),y=c(0,1), color="base"))+
+    scale_color_manual(values=c(ROC="blue", base="violet"))+
+    labs(color="model")
+  
+  
+  
+  print(roctest)
+  
+  #Return a weightMatrix
+  weightMatrix
+#}
+
+#run this big boy
+#weightMatrix <- main()
+
